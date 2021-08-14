8fb0d0481bc6a5c965637781da282b714db64a8f
Nikita Kuprins <nikita.kuprins@gmail.com>
2021-08-14 17:25:09 +0300

    Add original Casual MathCompanion

---
+++ MathCompanion/src/module-info.java
@@ -0,0 +1,8 @@
+module MathCompanion {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires org.junit.jupiter.api;
+    requires org.junit.jupiter.params;
+
+    opens com.nikitakuprins.mathCompanion;
+}
