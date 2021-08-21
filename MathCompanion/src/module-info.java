2268aa441ef7574e740611ebede97ab173ecfb2f
Nikita Kuprins <nikita.kuprins@gmail.com>
2021-08-21 14:34:55 +0200

    Delete all files

--- MathCompanion/src/module-info.java
+++
@@ -1,8 +0,0 @@
-module MathCompanion {
-    requires javafx.fxml;
-    requires javafx.controls;
-    requires org.junit.jupiter.api;
-    requires org.junit.jupiter.params;
-
-    opens com.nikitakuprins.mathCompanion;
-}
